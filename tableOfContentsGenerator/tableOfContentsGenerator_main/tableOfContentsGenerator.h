8a9159b5d58a2000bbd0a064b82ce0a3c7a93ad5
Egor <consicration@mail.ru>
2023-05-27 18:38:11 +0300

    Добавлена структура Header

--- tableOfContentsGenerator/tableOfContentsGenerator_main/tableOfContentsGenerator.h
+++ tableOfContentsGenerator/tableOfContentsGenerator_main/tableOfContentsGenerator.h
@@ -16,27 +16,9 @@ struct Header
 };
 
 /*!
-* \Структура для хранения информации о комментариях
-*/
-struct CommentedString
-{
-    QString rawData;
-    int startPos;
-    int endPos;
-};
-
-/*!
 * \Найти h заголовки в HTML-коде
 * \param[in] htmlCode - строка с HTML-кодом
 * \param[in,out] headersList - контейнер с h заголовками
 * \throw - выбрасывает исключения в случае ошибок
 */
 void findHeaders (const QString& htmlCode, QList<Header>& headersList);
-
-/*!
-* \Проверить на вложенность в друг друга h заголовки
-* \param[in] headerForCheck - заголовок для проверки
-* \param[in] nestedHeader - заголовок, куда будут сохраняться данные об вложенном заголовке
-* \return - результат выполнения функции (false - вложенные в друг друга заголовки не были найдены, true - вложенные в друг друга заголовки были найдены)
-*/
-bool checkForNestedHeaders (const Header& headerForCheck, Header& nestedHeader);
