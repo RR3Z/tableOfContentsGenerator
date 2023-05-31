0a5dbfdee76fc74c811ed6cedeeb916a5f18e192
Egor <consicration@mail.ru>
2023-05-31 21:07:02 +0300

    Добавлена реализация главной функции main

--- tableOfContentsGenerator/tableOfContentsGenerator_main/main.cpp
+++ tableOfContentsGenerator/tableOfContentsGenerator_main/main.cpp
@@ -1,8 +1,51 @@
 #include <QCoreApplication>
+#include "tableOfContentsGenerator.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    dataType inputDataType;
+    QString inputData;
+    QList<Header> headersList;
+    QString tableOfContents;
+
+    try
+    {
+        // Определить тип входных данных...
+        inputDataType = defineInputDataType(argc, argv);
+
+        // Если в качестве аргумента, переданного с запуском программы, был передан путь к файлу...
+        if (inputDataType == FILE_TYPE)
+        {
+            // Выгрузить содержимое файла в inputData
+            readInputDataFromFile(argv[1], inputData);
+        }
+
+        // Если в качестве аргумента, переданного с запуском программы, был передан URL-адрес...
+        if (inputDataType == URL_TYPE)
+        {
+            // Выгрузить HTML-код страницы в inputData
+            uploadHtmlCodeByUrl(argv[1], inputData);
+        }
+
+        // Найти в HTML-коде h заголовки...
+        findHeaders(inputData, headersList);
+
+        // Проверить контейнер с найденными h заголовками на пустое...
+        isListEmpty(headersList);
+
+        // Сгенерировать оглавление HTML-страницы...
+        generateTableOfContents(headersList, tableOfContents);
+
+        // Сохранить сгенерированное оглавление в файл...
+        saveDataToFile(tableOfContents);
+    }
+    catch (QString exceptionMessage)
+    {
+        // Вывести пользователю сообщение об ошибке
+        cout << exceptionMessage.toStdString() << endl;
+    }
 
     return a.exec();
 }
